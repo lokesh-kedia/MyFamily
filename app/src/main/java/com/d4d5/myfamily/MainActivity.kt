7e590e1b35c5f5da4c5d5dabe8c54f7c49a9d6fe
Lokesh kedia <lokesh.kedia04@gmail.com>
2021-09-09 13:32:16 +0530

    adding fragments

--- app/src/main/java/com/d4d5/myfamily/MainActivity.kt
+++ app/src/main/java/com/d4d5/myfamily/MainActivity.kt
@@ -14,18 +14,25 @@ class MainActivity : AppCompatActivity() {
 
         bottomBar.setOnItemSelectedListener { menuItem ->
 
-            if (menuItem.itemId == R.id.nav_guard) {
-
-                inflateFragment(GuardFragment.newInstance())
-
-            } else if (menuItem.itemId == R.id.nav_home) {
-
-                inflateFragment(HomeFragment.newInstance())
-
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    inflateFragment(HomeFragment.newInstance())
+                }
+                R.id.nav_dashboard -> {
+                    inflateFragment(DashboardFragment.newInstance())
+                }
+                R.id.nav_guard -> {
+                    inflateFragment(GuardFragment.newInstance())
+                }
+                R.id.nav_profile -> {
+                    inflateFragment(ProfileFragment.newInstance())
+                }
             }
 
             true
         }
+
+        bottomBar.selectedItemId = R.id.nav_home
 
     }
 
